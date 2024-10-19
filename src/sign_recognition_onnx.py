73ee053fc28bf08ec60da8f484d65e7d6028b0c0
andrei-radu <andrei_radu.danila@stud.etti.upb.ro>
2024-10-19 19:19:19 +0300

    added predict for onnx

--- src/sign_recognition_onnx.py
+++ src/sign_recognition_onnx.py
@@ -2,15 +2,65 @@ import numpy as np
 import onnxruntime as ort
 
 
+class HandSignRecognizer:
+    def __init__(self):
+        self.targets = {
+            1: "call",
+            2: "dislike",
+            3: "fist",
+            4: "four",
+            5: "like",
+            6: "mute",
+            7: "ok",
+            8: "one",
+            9: "palm",
+            10: "peace",
+            11: "rock",
+            12: "stop",
+            13: "stop inverted",
+            14: "three",
+            15: "two up",
+            16: "two up inverted",
+            17: "three2",
+            18: "peace inverted",
+            19: "no gesture",
+        }
+
+        self.model = self.__get_model()
+        self.input_name = self.model.get_inputs()[0].name
+        self.output_name = self.model.get_outputs()[0].name
+        
+
+    def __get_model(self, path='models/MobileNetV3FF_small.onnx', num_classes=18):
+        provider = ['CPUExecutionProvider']
+        ort_session = ort.InferenceSession(path, providers=provider)
+        return ort_session
+    
+    
+    def predict(self, x: np.ndarray):
+        x = x.astype(np.float32)
+        x = np.expand_dims(x, axis=0)
+        output = self.model.run([self.output_name], {self.input_name: x})
+        return output
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    
-    provider = ['TensorrtExecutionProvider']
-    ort_session = ort.InferenceSession("models/MobileNetV3FF_small.onnx", providers=provider)
-    
-    input_name = ort_session.get_inputs()[0].name
-    output_name = ort_session.get_outputs()[0].name
-    
-    input = np.random.randn(1, 3, 224, 224).astype(np.float32)
-    output = ort_session.run([output_name], {input_name: input})
+    model = HandSignRecognizer()
+    img = np.random.rand(3, 224, 224)
+    output = model.predict(img)
     
     print(output)
+    
