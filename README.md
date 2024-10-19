66273fb757500364539b93915f3f5dd0526cff62
andrei-radu <andrei_radu.danila@stud.etti.upb.ro>
2024-10-19 14:00:23 +0300

    new camera module and added download links

--- README.md
+++ README.md
@@ -12,11 +12,45 @@ To be added
 ## To Do
 - [ ] add mediapipe for hand tracking ?
 - [ ] clean the code
-- [ ] bash script for models download
+- [x] bash script for models download
 
 
 ## Installation
-To be added
+
+### Check that the camera is working
+
+First, check if the camera is working by running the following command:
+```bash
+gst-launch-1.0 nvarguscamerasrc-1.0 ! nvoverlaysink
+```
+
+If the camera is working, you should see a window with the camera feed. Press `Ctrl+C` to close the window.
+
+If the camera is not working, make sure the pins are configured correctly. Use the following command to check the pins:
+```bash
+sudo /opt/nvidia/jetson-io/jetson-io.py
+```
+
+
+### Clone the repository
+Jetson Nano should already have the git-cli installed. If not, run the following command:
+```bash
+sudo apt-get install git
+```
+
+Then clone the repository:
+```bash
+git clone https://github.com/andrei-radu/iasi-rps.git
+```
+
+
+### Download the model
+Run the following command in the root directory of the project to download the model:
+```bash
+wget -O models/MobileNetV3FF_small.pth 'https://ctipub-my.sharepoint.com/:u:/g/personal/andrei_radu_danila_stud_etti_upb_ro/EZXgxLpmO7hOt4GqmMwleXQBE509Mq81vP7F6khJg4MgaQ?e=0VA5NQ&download=1'
+```
+
+
 
 
 ## Acknowledgments
--- src/camera.py
+++ src/camera.py
@@ -1,33 +1,89 @@
 import cv2
+import numpy as np
 
 
 class Camera:
-    def __init__(self) -> None:
-        self.cam = cv2.VideoCapture(0)
-        self.cam.set(cv2.CAP_PROP_FPS, 24)
-        _, _ = self.cam.read()  # activate camera
+    """Class for handling camera operations."""
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        device_id: int = 0, # 0 for CAM0 port, 1 for CAM1 port
+        flip: bool = False, # Flip the image by 180 degrees
+        width: int = 640, # Width of the image, in pixels
+        height: int = 480, # Height of the image, in pixels
+        fps: int = 30, # Frames per second
+    ):
+        """Initialize the camera object properties.
 
-    def get_image(self):
-        _, img = self.cam.read()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return img
-    
-    def close_camera(self):
-        self.cam.release
+        Args:
+            device_id (int, optional): ID of device, either 0 or 1, corresponding to CAM port. Defaults to 0.
+            flip (bool, optional): Whether to flip the image by 180 degrees. Defaults to False.
+            width (int, optional): Width of the image, in pixels. Defaults to 640.
+            height (int, optional): Height of the image, in pixels. Defaults to 480.
+            fps (int, optional): Frames per second. Defaults to 30.
+        """
+        self.device_id = device_id
+        self.flip = flip
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.cap = None  # placeholder for the camera object
+
+
+    def start(self) -> None:
+        """Open the camera and start capturing frames. 
+        
+        Raises:
+            RuntimeError: If the camera could not be opened.
+        """
+        pipeline = (
+            f"gst-launch-1.0 nvarguscamerasrc sensor-id={self.device_id} ! "
+            "nvoverlaysink ! "
+            "video/x-raw(memory:NVMM), "
+            f"width={self.width}, "
+            f"height={self.height}, "
+            "format=(string)NV12, "
+            f"framerate={self.fps}/1 ! "
+            "nvvidconv flip-method={(2 if self.flip else 0)} ! "
+            "video/x-raw, format=(string)BGRx ! videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+        )
+        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+        if not self.cap.isOpened():
+            raise RuntimeError(f"Could not open camera on CAM{self.device_id}")
+        _, _ = self.cap.read()  # activate camera
+
+    def get_image(self) -> np.ndarray:
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        _, image = self.cap.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+
+
+    def stop(self) -> None:
+        """Stop capturing frames and release the camera."""
+        if self.cap is not None:
+            self.cap.release()
+            self.cap = None
+        
 
 
 if __name__ == '__main__':
     import time
     import matplotlib.pyplot as plt
     
-    camera = Camera()
-
-    time.sleep(1.5)  # stabilize sensor
-
-    image = camera.get_image()
-
-    print(f"Image shape is {image.shape}")
-
-    plt.figure()
-    plt.imshow(image)
+    cam = Camera()
+    cam.start()
+    time.sleep(2)
+    
+    img = cam.get_image()
+    
+    plt.imshow(img)
     plt.show()
+    
+    cam.stop()
+    
+    print("Done!")
